b7fba7900b5f7332930007e464db15af22d40c44
Mahathi Jayam <36083843+MahathiJayam@users.noreply.github.com>
2018-03-02 19:54:59 +0530

    Initial commit

---
+++ my/includes/layouts/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>R2</title>
+	<link href="css/public.css" media="all" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<div id="header">
+<h1>R2</h1>
+</div>
